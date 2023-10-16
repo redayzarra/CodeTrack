1cb29c4f6b483ce41dd7c34f280da26ce36d7d89
ReDay Zarra <redayzarra@gmail.com>
2023-10-15 23:56:28 -0400

    Adding dynamic links to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
-  
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -11,17 +14,15 @@ const NavBar = () => {
         <AiFillBug />
       </Link>
       <ul className="flex space-x-6">
-        <li>
+        {links.map((link) => (
           <Link
-            href="/"
+            key={link.href}
+            href={link.href}
             className="text-zinc-500 hover:text-zinc-800 transition-colors"
           >
-            Dashboard
+            {link.label}
           </Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        ))}
       </ul>
     </nav>
   );

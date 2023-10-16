145c47bc599608aee2006de1a3129088feb86dd9
ReDay Zarra <redayzarra@gmail.com>
2023-10-15 23:53:33 -0400

    Styling basic navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  
+
   return (
-    <nav>
-      <Link href="/">Logo</Link>
-      <ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
         <li>
-          <Link href="/">Dashboard</Link>
+          <Link
+            href="/"
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            Dashboard
+          </Link>
         </li>
         <li>
           <Link href="/issues">Issues</Link>

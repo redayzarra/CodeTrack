a43a8854e9d9bce0f0e6bff4ffa32a1c71a2d43a
ReDay Zarra <redayzarra@gmail.com>
2023-10-17 11:39:44 -0400

    Organizing import statments

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Link } from "./Link";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as Spinner } from "./IssueStatusBadge";
+export { default as IssueStatusBadge } from "./Spinner";
+export { default as Skeleton } from "./Skeleton";
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import IssueActions from "./IssueActions";
 import Link from "../components/Link";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();

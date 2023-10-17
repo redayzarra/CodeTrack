3b92ddd8ec35a185d30eb0c46ff68af3b17b7c42
ReDay Zarra <redayzarra@gmail.com>
2023-10-17 07:10:16 -0400

    Backend works perfectly with React Hook forms

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,6 +5,7 @@ import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface IssueForm {
   title: string;
@@ -12,6 +13,8 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+  const router = useRouter();
+
   const { register, control, handleSubmit } = useForm<IssueForm>();
 
   return (
@@ -19,6 +22,7 @@ const NewIssuePage = () => {
       className="max-w-l space-y-3"
       onSubmit={handleSubmit(async (data) => {
         await axios.post("/api/issues", data);
+        router.push("/issues");
       })}
     >
       <TextField.Root>

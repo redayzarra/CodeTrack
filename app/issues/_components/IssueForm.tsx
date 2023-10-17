9e664cf474a020c585d387c897e9ee510e0e1ed7
ReDay Zarra <redayzarra@gmail.com>
2023-10-17 18:22:41 -0400

    Fixing issue form loading skeleton

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,16 +7,12 @@ import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { FaCheckCircle } from "react-icons/fa";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 // Type for the form data
 type IssueFormData = z.infer<typeof issueSchema>;
@@ -53,7 +49,6 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       }
       router.push("/issues");
       router.refresh();
-      
     } catch (error) {
       setSubmitting(false);
       setError("A strange error occurred... We're looking into it.");

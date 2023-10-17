c85f409efb5bbdc912514846085eb79fb3aecb4f
ReDay Zarra <redayzarra@gmail.com>
2023-10-17 11:00:17 -0400

    Adding skeleton for issue detail page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card, Box, Badge } from "@radix-ui/themes";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <Box className="max-w-xl mt-[5px]">
+      <Skeleton width="13rem" />
+      <Flex gap="3" my="2">
+        <Badge color="gray">$$$$</Badge>
+        <Skeleton width="13.5rem" />
+      </Flex>
 
-export default LoadingIssueDetailPage
+      <Card className="mt-[26px]">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
+};
+
+export default LoadingIssueDetailPage;

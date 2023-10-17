0d3d9bbae5c36cffada654db0cb948f812c83873
ReDay Zarra <redayzarra@gmail.com>
2023-10-17 09:52:02 -0400

    Fixing loading bug and showing issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
 import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
@@ -31,7 +28,7 @@ const IssuesPage = async () => {
             {issues.map((issue) => (
               <Table.Row key={issue.id}>
                 <Table.Cell>
-                  {issue.title}
+                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                   <div className="block md:hidden">
                     <IssueStatusBadge status={issue.status} />
                   </div>

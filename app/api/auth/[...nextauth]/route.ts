e874f8a5c911b2b7a3103d5e15c483e44fa7d2e0
ReDay Zarra <redayzarra@gmail.com>
2023-10-18 19:37:10 -0400

    Fixing prisma adapter and nextauth.js bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };

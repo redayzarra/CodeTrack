3abcca136f6a40abc6ba9bf784c133b4598c4051
ReDay Zarra <redayzarra@gmail.com>
2023-10-19 11:26:46 -0400

    Dividing navbar into simpler components

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,14 +10,6 @@ import { Avatar, Box, Container, DropdownMenu, Text } from "@radix-ui/themes";
 import { Spinner } from "./components";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5">
       <Container>
@@ -26,64 +18,83 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classNames({
-                      "text-zinc-900 font-medium": link.href == currentPath,
-                      "text-zinc-500": link.href != currentPath,
-                      "hover:text-zinc-800 transition-all": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </div>
 
-          <div>
-            <Box>
-              {status == "authenticated" && (
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Avatar
-                      src={session.user!.image!}
-                      fallback="?"
-                      size="3"
-                      radius="full"
-                      className="cursor-pointer"
-                      referrerPolicy="no-referrer"
-                    />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    <DropdownMenu.Label>
-                      <Text size="2">{session.user?.email}</Text>
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Item>
-                      <Link href="/api/auth/signout">Log Out</Link>
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-              )}
-
-              {status == "loading" && (
-                <div className="pr-3">
-                  <Spinner />
-                </div>
-              )}
-
-              {status == "unauthenticated" && (
-                <Link href="/api/auth/signin">Log In</Link>
-              )}
-            </Box>
-          </div>
+          <AuthStatus />
         </div>
       </Container>
     </nav>
   );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              "text-zinc-900 font-medium": link.href == currentPath,
+              "text-zinc-500": link.href != currentPath,
+              "hover:text-zinc-800 transition-all": true,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status == "unauthenticated") {
+    return <Link href="/api/auth/signin">Log In</Link>;
+  } else if (status == "loading") {
+    return (
+      <div className="p-3">
+        <Spinner />
+      </div>
+    );
+  } else {
+    return (
+      <Box>
+        {status == "authenticated" && (
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar
+                src={session.user!.image!}
+                fallback="?"
+                size="3"
+                radius="full"
+                className="cursor-pointer"
+                referrerPolicy="no-referrer"
+              />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label>
+                <Text size="2">{session!.user!.email}</Text>
+              </DropdownMenu.Label>
+              <DropdownMenu.Item>
+                <Link href="/api/auth/signout">Log Out</Link>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        )}
+      </Box>
+    );
+  }
 };
 
 export default NavBar;

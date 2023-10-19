8d2b8b5b77a2ca690906d6fcc4d62fa101d1acc7
ReDay Zarra <redayzarra@gmail.com>
2023-10-18 22:05:17 -0400

    Fixing Edit button bug

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -1,11 +1,16 @@
 import { Pencil1Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { Button, Text } from "@radix-ui/themes";
 import Link from "next/link";
 
 const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
     <Button>
-      <Pencil1Icon /> <Link href={`/issues/edit/${issueId}`}>Edit</Link>
+      <Link
+        href={`/issues/edit/${issueId}`}
+        className="flex items-center space-x-2"
+      >
+        <Pencil1Icon /> <Text>Edit</Text>
+      </Link>
     </Button>
   );
 };

cea4636a18935d3013693c42a618750d336ef4e4
ReDay Zarra <redayzarra@gmail.com>
2023-10-22 20:00:28 -0400

    Allowing multiple sorting filters

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value: Status | "ALL" }[] = [
@@ -14,12 +14,23 @@ const statuses: { label: string; value: Status | "ALL" }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
       defaultValue="ALL"
       onValueChange={(status) => {
-        const query = status != "ALL" ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+
+        if (status != "ALL") {
+          params.append("status", status);
+        }
+
+        if (searchParams.get("orderBy")) {
+          params.append("orderBy", searchParams.get("orderBy")!);
+        }
+
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >

a06d04b1a11e1696022cda527214fb1dc38cdedb
ReDay Zarra <redayzarra@gmail.com>
2023-10-24 07:15:58 -0400

    Changing relation mode to Prisma

--- prisma/migrations/20231021084803_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `issue` DROP FOREIGN KEY `Issue_assignedToUserId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
--- prisma/migrations/20231021084951_trying/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `Account_userId_fkey` ON `account`;
-
--- DropIndex
-DROP INDEX `Issue_assignedToUserId_fkey` ON `issue`;
-
--- DropIndex
-DROP INDEX `Session_userId_fkey` ON `session`;
